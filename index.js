3b1967834940b2b0781c9ad317a4c0bf96c687f0
Olamide Akomolafe <olamideakomolafe1234@gmail.com>
2020-05-24 16:28:24 +0300

    more last ditch efforts

--- index.js
+++ index.js
@@ -2,25 +2,13 @@
 const fastify = require('fastify')({
     logger: true
 })
-// const cors = require('cors')
+const cors = require('cors')
 require('dotenv').config()
 
 
 var db = process.env.MONGODB_URL
 
-//fastify.use(cors())
-fastify.all('/*', function(req, res, next) {
-  // CORS headers
-  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  // Set custom headers for CORS
-  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
-  if (req.method == 'OPTIONS') {
-    res.status(200).end();
-  } else {
-    next();
-  }
-});
+fastify.use(cors())
 
 const mongoose = require('mongoose') 
 
