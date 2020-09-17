8184e6c5e950d907bcf98b98dfc8b7f451458117
wachukxs <nwachukwuossai@gmail.com>
2020-09-17 10:26:56 +0300

    bug fix for transaction save failure

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -28,10 +28,8 @@ exports.createTransaction = async (req, reply) => {
                        to: email,
                        subject: "You just got tipped " + req.body.username.capitalize(),
                        generateTextFromHTML: true,
-                       html: "<h2>Hi <b>"+ req.body.username.capitalize() + ",</b></h2>" +
-                       (req.body.tx_ref.includes('shukraning-') ? 
-                       "You just got a new shuclan, " + req.body.supporter_nickname :
-                       + req.body.supporter_nickname + " just tipped you!") + "<br>"
+                       html: "<h2>Hi <b>" + req.body.username.capitalize() + "</h2>,</b>"
+                       + req.body.supporter_nickname + " just tipped you!" + "<br>"
                        + "<a href='https://useshukran.com/accounts'>Login to find out how much.</a>"
                        };
 
